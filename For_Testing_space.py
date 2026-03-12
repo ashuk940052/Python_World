88ed92bc315e79130b715b44528b296c032e4367
ashuk940052 <ec19101@global.org.in>
2026-03-12 19:59:52 +0530

    init commit

---
+++ For_Testing_space.py
@@ -0,0 +1,14 @@
+"""
+print("Hello", 84, 'end', False, end='\n')
+print("Hello world")
+s = input("Name:- ")
+age = input('age :- ')
+print(s , age) """
+
+x = "awesome"
+
+def myfunction():
+
+    print("the python is " , x)
+
+myfunction()
